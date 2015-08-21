d89ebf8733851c8b718ea900313f2b64a8a7ad85
zkan <kan@prontomarketing.com>
2015-08-21 19:44:05 +0700

    complete fizz scenario

--- spec/fizzbuzz.spec.js
+++ spec/fizzbuzz.spec.js
@@ -1,7 +1,17 @@
 describe("FizzBuzz", function() {
+    var app;
+
+    beforeEach(function() {
+        app = require("../app/fizzbuzz");
+    });
+
     it("should return fizz if input is 3", function() {
-        var app = require("../app/fizzbuzz");
         result = app.fizzbuzz(3);
+        expect(result).toBe("fizz");
+    });
+
+    it("should return fizz if input is 6", function() {
+        result = app.fizzbuzz(6);
         expect(result).toBe("fizz");
     });
 });
